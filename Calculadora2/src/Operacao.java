77b981133b99fce18e7d106863e38f3046880487
ellenrayzer <ellenrayzer@hotmail.com>
2018-07-31 08:54:14 -0300

    Adicionadas Strings para digitar os números usados na conta

--- Calculadora2/src/Operacao.java
+++ Calculadora2/src/Operacao.java
@@ -1,4 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class Operacao {
-
+	 public static void main(String[] args) {
+	      
+	      String numeroA = JOptionPane.showInputDialog(null, "Digite um número:");
+	      String numeroB = JOptionPane.showInputDialog(null, "Digite o segundo número:");
+	      
+	 }    
 }
